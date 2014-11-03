a0a5ef40001964e62fb391dba7c78d067f67932d
BeyondCloud <a1989796@yahoo.com.tw>
2014-11-03 18:14:47 +0800

    BallGame UnityProj
    practice

---
+++ Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreSystem : MonoBehaviour {
+
+
+	public static int score;
+	Text text;
+	// Update is called once per frame
+	void Awake()
+	{
+		text = GetComponent<Text> ();
+
+		score = 0;
+	}
+	void Update()
+	{
+		text.text = "Score:" + score;
+	}
+
+}
